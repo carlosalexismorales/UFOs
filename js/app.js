272a41d51d32c4f5ded2065e31620a8759c75197
Carlos Morales <carlosmorales@Carloss-MacBook-Pro-2.local>
2022-08-08 21:35:29 -0700

    adding buildthefinaltable code to app.js file

--- js/app.js
+++ js/app.js
@@ -22,9 +22,9 @@ function buildTable(data) {
         }
       );
     });
-  }  
+}  
 
-  function handleClick() {
+function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
@@ -35,10 +35,16 @@ function buildTable(data) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
   
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
-  };
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
